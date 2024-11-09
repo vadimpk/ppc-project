b31d0130cff6d3866e3e804e90595e4b33df428e
vadimpk <vadyman.pk@gmail.com>
2024-11-09 23:30:42 +0200

    oh shi forgot this file lol

--- api/services/business_service.go
+++ api/services/business_service.go
@@ -1,1 +1,42 @@
 package services
+
+import (
+	"context"
+
+	"github.com/vadimpk/ppc-project/entity"
+	"github.com/vadimpk/ppc-project/repository"
+)
+
+type businessServiceService struct {
+	repos *repository.Repositories
+}
+
+func NewBusinessServiceService(repos *repository.Repositories) BusinessServiceService {
+	return &businessServiceService{
+		repos: repos,
+	}
+}
+
+func (s *businessServiceService) Create(ctx context.Context, service *entity.BusinessService) error {
+	panic("not implemented")
+}
+
+func (s *businessServiceService) Get(ctx context.Context, id int) (*entity.BusinessService, error) {
+	panic("not implemented")
+}
+
+func (s *businessServiceService) Update(ctx context.Context, service *entity.BusinessService) error {
+	panic("not implemented")
+}
+
+func (s *businessServiceService) Delete(ctx context.Context, id int) error {
+	panic("not implemented")
+}
+
+func (s *businessServiceService) List(ctx context.Context, businessID int) ([]entity.BusinessService, error) {
+	panic("not implemented")
+}
+
+func (s *businessServiceService) ListActive(ctx context.Context, businessID int) ([]entity.BusinessService, error) {
+	panic("not implemented")
+}
